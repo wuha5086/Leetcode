4349c18d390b1d20c31cce87cfeb9c9eda844ecb
Han <31714576+wuha5086@users.noreply.github.com>
2019-11-30 17:04:44 -0500

    Create project-employees-i.sql

---
+++ Database/project-employees-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT 
+    project_id, 
+    ROUND(AVG(experience_years), 2) AS average_years
+FROM Project 
+JOIN Employee USING (employee_id)
+GROUP BY project_id; 
