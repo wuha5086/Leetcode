eff58ad560650aba5928d40779600a46f20c179c
Han <wuha5086@hotmail.com>
2020-02-24 23:53:14 -0500

    Update game-play-analysis-i.sql

--- Database/game-play-analysis-i.sql
+++ Database/game-play-analysis-i.sql
@@ -5,7 +5,16 @@ FROM Activity
 GROUP BY player_id;
 
 /* Write your T-SQL query statement below */
+# Time out
 SELECT DISTINCT(player_id), MIN(event_date)
 OVER(PARTITION BY player_id) AS first_login
 FROM Activity
+
+/* Write your T-SQL query statement below */
+SELECT player_id, event_date as first_login
+FROM(SELECT player_id, event_date,
+    RANK() OVER(PARTITION BY player_id ORDER BY event_date) AS date_rank
+    FROM Activity
+    ) temp
+WHERE date_rank = 1
 
