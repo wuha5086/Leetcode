c28cd401b0f68832e04da7ffe513e438e99c62cd
Han <wuha5086@hotmail.com>
2020-02-24 23:50:06 -0500

    Update game-play-analysis-i.sql

--- Database/game-play-analysis-i.sql
+++ Database/game-play-analysis-i.sql
@@ -3,3 +3,9 @@ SELECT player_id,
        MIN(event_date) AS first_login
 FROM Activity
 GROUP BY player_id;
+
+/* Write your T-SQL query statement below */
+SELECT DISTINCT(player_id), MIN(event_date)
+OVER(PARTITION BY player_id) AS first_login
+FROM Activity
+
