d280105b8683bc722dcd88f69c454f50d32cc046
Han <wuha5086@hotmail.com>
2020-06-13 15:16:55 -0400

    Update sales-analysis-i.sql

--- Database/sales-analysis-i.sql
+++ Database/sales-analysis-i.sql
@@ -15,6 +15,20 @@ HAVING
 		ORDER BY pr DESC
 		LIMIT 1
 	);
+
+/* Write your T-SQL query statement below */
+WITH CTE AS(
+		SELECT seller_id, SUM(price) AS totalSales
+		FROM Sales
+		GROUP BY seller_id
+    )
+SELECT seller_id
+FROM cte
+WHERE totalSales = (SELECT MAX(totalSales) FROM cte)
+
+
+
+
     
     
     
