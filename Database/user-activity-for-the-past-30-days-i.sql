7b4e4cc18a0c7438643424257534ee2010819738
Han <wuha5086@hotmail.com>
2020-06-14 17:29:16 -0400

    Update user-activity-for-the-past-30-days-i.sql

--- Database/user-activity-for-the-past-30-days-i.sql
+++ Database/user-activity-for-the-past-30-days-i.sql
@@ -4,6 +4,12 @@ FROM Activity
 WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
 GROUP BY day;
 
+# Write your MySQL query statement below
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE_SUB(DATE("2019-07-27"), INTERVAL 29 DAY) AND DATE("2019-07-27") 
+GROUP BY activity_date; 
+
 /* Write your T-SQL query statement below */
 SELECT activity_date AS day,
        COUNT(DISTINCT user_id) AS active_users
