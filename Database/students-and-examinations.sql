cee466fa54f9f2964c374d348281a00b973db42e
Han <wuha5086@hotmail.com>
2020-06-14 11:21:49 -0400

    Update students-and-examinations.sql

--- Database/students-and-examinations.sql
+++ Database/students-and-examinations.sql
@@ -12,5 +12,15 @@ FROM
 GROUP BY s.student_id, j.subject_name
 ORDER BY s.student_id, j.subject_name;
 
+# Write your MySQL query statement below
+
+SELECT s.student_id,s.student_name,j.subject_name,COUNT(e.subject_name) as attended_exams
+FROM Students as s
+JOIN Subjects as j
+LEFT JOIN Examinations as e
+ON s.student_id=e.student_id AND j.subject_name=e.subject_name
+GROUP BY s.student_id,j.subject_name
+ORDER BY s.student_id,j.subject_name;
+
 
 
