2e08e3ddda4609dfd93f94e6d403ebb879adc274
Han <wuha5086@hotmail.com>
2020-06-14 11:22:12 -0400

    Create ads-performance.sql

---
+++ Database/ads-performance.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT ad_id,
+    IFNULL(ROUND(AVG(
+    CASE 
+       WHEN action = 'Clicked' THEN 1
+       WHEN action = 'Viewed' THEN 0
+       ELSE NULL  
+    END)*100,2),0) AS ctr
+FROM Ads
+GROUP BY ad_id
+ORDER BY ctr DESC, ad_id ASC;
