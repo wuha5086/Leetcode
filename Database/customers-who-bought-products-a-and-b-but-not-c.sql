088e6bda99377a14b87c3253c0ecbe4f831f0806
Han <wuha5086@hotmail.com>
2020-06-18 12:58:50 -0400

    Update customers-who-bought-products-a-and-b-but-not-c.sql

--- Database/customers-who-bought-products-a-and-b-but-not-c.sql
+++ Database/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 SELECT c.customer_id, c.customer_name
-FROM Customers AS c JOIN Orders AS o
+FROM Customers c
+JOIN Orders o
 ON c.customer_id = o.customer_id
-GROUP BY customer_id
-HAVING SUM(CASE WHEN product_name = 'A' THEN 1 ELSE 0 END) > 0
-AND SUM(CASE WHEN product_name = 'B' THEN 1 ELSE 0 END) > 0
-AND SUM(case WHEN product_name = 'C' THEN -1 ELSE 0 END) >= 0;
+GROUP BY c.customer_id
+HAVING SUM(CASE WHEN o.product_name = 'A' THEN 1 ELSE 0 END) > 0 AND
+SUM(CASE WHEN o.product_name = 'B' THEN 1 ELSE 0 END) > 0 AND
+SUM(CASE WHEN o.product_name = 'C' THEN -1 ELSE 0 END) = 0;
