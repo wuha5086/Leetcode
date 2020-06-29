c4dad82c11b8986fb4dc81ace01540abd4641f38
Han <wuha5086@hotmail.com>
2020-06-29 17:08:38 -0400

    Update project-employees-iii.sql

--- Database/project-employees-iii.sql
+++ Database/project-employees-iii.sql
@@ -1,20 +1,11 @@
-SELECT 
-    project_id, 
-    employee_id 
-FROM 
-    Project a
-    JOIN Employee b USING (employee_id)
-    JOIN (SELECT project_id, MAX(experience_years) AS max_years 
-          FROM Project JOIN Employee USING (employee_id) 
-          GROUP BY project_id) c USING (project_id)
-WHERE b.experience_years = c.max_years; 
-
-#Write your MySQL query statement below
-SELECT 
-    project_id, 
-    employee_id 
-FROM 
-    Project a
-    JOIN Employee USING (employee_id)
-WHERE 
-    experience_years = (SELECT MAX(experience_years) FROM Project JOIN Employee USING (employee_id) WHERE project_id = a.project_id);
+# Write your MySQL query statement below
+SELECT p.project_id, e.employee_id
+FROM Project p
+JOIN Employee e
+ON p.employee_id = e.employee_id
+WHERE e.experience_years =
+(SELECT MAX(e1.experience_years) as exp
+FROM Project p1
+JOIN Employee e1
+ON p1.employee_id = e1.employee_id
+WHERE p.project_id = p1.project_id)
