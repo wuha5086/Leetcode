47e85c5f983579182e711e8003019528174bf690
Han <wuha5086@hotmail.com>
2020-06-30 09:06:35 -0400

    Update project-employees-iii.sql

--- Database/project-employees-iii.sql
+++ Database/project-employees-iii.sql
@@ -17,11 +17,11 @@ SELECT
 FROM 
     Project p
     JOIN Employee e 
-    ON p.employee_id = e.employee_id
     JOIN (SELECT p1.project_id, MAX(experience_years) AS max_years 
           FROM Project p1 
           JOIN Employee e1 
           ON p1.employee_id = e1.employee_id
           GROUP BY p1.project_id) tmp
+    ON p.employee_id = e.employee_id
     ON p.project_id = tmp.project_id
 WHERE e.experience_years = tmp.max_years; 
