e07f940768d6b0b8ea3a80d5e2b657269aded3b8
Han <wuha5086@hotmail.com>
2020-07-03 21:56:35 -0400

    Update number-of-trusted-contacts-of-a-customer.sql

--- Database/number-of-trusted-contacts-of-a-customer.sql
+++ Database/number-of-trusted-contacts-of-a-customer.sql
@@ -1,14 +1,22 @@
 # Write your MySQL query statement below
-SELECT invoice_id, 
-       customer_name, 
-       price, 
-       COUNT(contact_name) AS contacts_cnt,
-       SUM(CASE WHEN contact_email IN (SELECT email FROM Customers) THEN 1
-       ELSE 0 END) AS trusted_contacts_cnt
-FROM Invoices
-INNER JOIN Customers
-ON Invoices.user_id = Customers.customer_id
-LEFT JOIN Contacts
-ON Customers.customer_id = Contacts.user_id
-GROUP BY invoice_id
-ORDER BY invoice_id;
+WITH a AS
+(SELECT user_id, COUNT(*) AS trusted_contacts_cnt
+FROM contacts
+WHERE contact_email IN (SELECT email FROM customers)
+GROUP BY user_id),
+b AS
+(SELECT user_id, COUNT(contact_email) AS contacts_cnt
+FROM contacts
+GROUP BY user_id)
+
+SELECT 
+    invoice_id, 
+    customer_name, 
+    price, 
+    ifnull(contacts_cnt,0) AS contacts_cnt,
+    ifnull(trusted_contacts_cnt,0) AS trusted_contacts_cnt
+FROM invoices i 
+LEFT JOIN customers c ON i.user_id=c.customer_id
+LEFT JOIN a ON i.user_id=a.user_id
+LEFT JOIN b ON i.user_id=b.user_id
+ORDER BY invoice_id
