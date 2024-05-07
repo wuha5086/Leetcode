2004be7d76be16edcf7c0974c07a982f46c38fa4
Han <wuha5086@hotmail.com>
2024-05-07 23:31:20 +0800

    abs value first, if exist negative index, then it is duplicate

---
+++ Algorithms/442_findDuplicates.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
+        for n in nums:
+            n = abs(n)
+            if nums[n-1] < 0:
+                res.append(n)
+            else:
+                nums[n-1] = - nums[n-1]
+        return res
